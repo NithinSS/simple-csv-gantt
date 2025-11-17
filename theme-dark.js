9fc71447fbcd3ba0317d8e853471bd17d54751f5
Nithin S Sabu <nsabu@orthofx.com>
2025-11-17 15:45:20 +0530

    theming code, final touches

---
+++ theme-dark.js
@@ -0,0 +1,33 @@
+const THEME = {
+    backgroundColor: {
+        fill: '#B5CCDA'
+    },
+    gantt: {
+    trackHeight: 35,
+    barHeight: 20,
+    barCornerRadius: 10,
+    shadowEnabled: true,
+    palette: [
+        { color: '#717D9D', dark: '#444F6C', light: '#717D9D' },
+        { color: '#B5CCDA', dark: '#8AB4CD', light: '#B5CCDA' },
+        { color: '#ECCAC8', dark: '#E3B1B3', light: '#ECCAC8' },
+        { color: '#C8D7DE', dark: '#9AA7AC', light: '#C8D7DE' },
+    ],
+    arrow: {
+        width: 3,
+        color: '#B5CCDA'
+    },
+    innerGridHorizLine: {
+        stroke: 'transparent',
+        strokeWidth: 1
+    },
+    innerGridTrack: {fill: '#151635'},
+    innerGridDarkTrack: {fill: '#15162B'},
+    defaultStartDate: new Date(),
+    labelStyle: {
+        fontName: 'helvetica',
+        fontSize: 14,
+        color: 'white'
+    },
+    }
+}
